4aac106b672376f2adf3b1d2cb222c6d6624249c
DavidMekhtiev <mekhtiev.1995@bk.ru>
2024-06-17 12:55:14 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = "Host=localhost;Port=5432;Database=BankConnection_temp;username=postgres;Password=123456;";
+
+        NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+
+        using NpgsqlCommand updateCmd = new NpgsqlCommand("UPDATE public.\"IntegrationHistory\" SET \"IsSend\" = false WHERE \"ResponseContent\" LIKE '%xml version%'", connection);
+
+        try
+        {
+            int rowsAffected = updateCmd.ExecuteNonQuery();
+            Console.WriteLine("Checked and updated all bad responses");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unnable to check bad response");
+            throw new Exception(ex.Message);
+        }
+
+    }
+}
